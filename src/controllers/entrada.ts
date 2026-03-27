9ceafdcebb02c8dde70943a7a0bede253dad442e
Kass-Lopes <kasslopes0@gmail.com>
2026-03-27 21:19:44 +0100

    implementando controllers

---
+++ src/controllers/entrada.ts
@@ -0,0 +1,10 @@
+import { I_EntradaController } from "./protoclos";
+
+export class Entrada implements I_EntradaController {
+    handle() {
+        throw new Error("Method not implemented.");
+
+        //validar requisição
+        //mandar a requição para o repository
+    }
+}
---
+++ src/controllers/protoclos.ts
@@ -0,0 +1,3 @@
+export interface I_EntradaController {
+    handle(): any
+}
