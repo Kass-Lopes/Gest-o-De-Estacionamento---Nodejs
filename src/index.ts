249625edcdf1771864c200f68e0dad0a12982ad4
Kass-Lopes <kasslopes0@gmail.com>
2026-03-27 17:27:40 +0100

    add servideor http com express

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from "express"
+import { config } from "dotenv"
+
+config()
 
 const app = express()
+const port = process.env.PORT || "8082"
 app.use(express.json())
 
 app.get("/", (req, res)=>{
     res.json("Duto pronto!")
 })
 
-app.listen("8000", ()=> {console.log("All Ok")})
+app.listen(port, ()=> {
+    console.log("All Ok")
+})
