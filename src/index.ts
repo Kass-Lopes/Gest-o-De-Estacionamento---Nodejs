325b0bfa56f3bd5d6d31562437d6a8dc96779aca
Kass-Lopes <kasslopes0@gmail.com>
2026-04-19 23:16:00 +0100

    actualizando alguns arquivos

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import express from "express"
 import { config } from "dotenv"
-
+import { router } from "./routes"
+ 
 config()
 
 const app = express()
 const port = process.env.PORT || "8082"
 app.use(express.json())
+app.use(router)
 
-app.get("/", (req, res)=>{
-    res.json("Duto pronto!")
-})
+
+// app.get("/", (req, res)=>{
+//     res.json("servidor pronto!")
+// })
 
 app.listen(port, ()=> {
-    console.log("All Ok")
+    console.log("Servidor rodando em http://localhost:"+port)
 })
