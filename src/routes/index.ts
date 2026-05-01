0ae271a646dc7b0dd868781e31183bdb980428b3
Kass-Lopes <kasslopes0@gmail.com>
2026-05-01 19:10:15 +0100

    criando a primeira rota para buscar vagas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,23 @@
 import { Router } from "express"
+import { BuscaVagasController } from "../controllers/busca-vagas/busca-vagas.controller"
+import { BuscaVagasRepository } from "../repository/busca-vagas-repo/busca-vagas-repo"
+import { conectarDB, criarTabelas } from "../config/sqlite"
 
 const router = Router()
 
-router.get("/api/parking/check-in", (req, res)=>{
-    res.send("fazendo check-in de vagas!")
-})
+const main =  async ()=>{
+    
+    const db = await conectarDB()
+    await criarTabelas(db)
 
-export { router }
+    router.get("/api/vagas", async (req, res)=>{
+        
+        const buscaVagaDB = new BuscaVagasRepository(db)
+        const buscaVaga = new BuscaVagasController(buscaVagaDB)
+    
+        const {status, data} = await buscaVaga.handle()
+        res.send({status,data})
+    })
+}
+
+export { router, main }
