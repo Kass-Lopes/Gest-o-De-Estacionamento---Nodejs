dfd1d0c7d73079484f7bac1898290300b287fd80
Kass-Lopes <kasslopes0@gmail.com>
2026-05-01 19:13:37 +0100

    chamando a main de rotas

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,13 @@
 import express from "express"
 import { config } from "dotenv"
-import { router } from "./routes"
+import { main, router } from "./routes"
  
 config()
-
+main()
 const app = express()
 const port = process.env.PORT || "8082"
 app.use(express.json())
 app.use(router)
-
-
-// app.get("/", (req, res)=>{
-//     res.json("servidor pronto!")
-// })
 
 app.listen(port, ()=> {
     console.log("Servidor rodando em http://localhost:"+port)
