2e5f3e236edb2dd49f5944ccdd5c62fb81928fc2
Kass-Lopes <kasslopes0@gmail.com>
2026-05-03 18:29:48 +0100

    HTTP REQUEST

--- src/controllers/protocols.ts
+++ src/controllers/protocols.ts
@@ -2,3 +2,20 @@ export interface HttpResponse<T> {
     status: number,
     data: T | string
 }
+
+export interface HttpRequest {
+    
+}
+
+export interface vagas{
+    id: number,
+    status: string
+}
+
+export interface ticket {
+    id: string,
+    placa: string,
+    vaga_id: string,
+    hora_de_entrada?: Date,
+    hora_de_saide?: Date
+}
