d988645fa410c7868674e28fdf5f82f321f7e7bd
Kass-Lopes <kasslopes0@gmail.com>
2026-05-03 20:50:27 +0100

    Repository para a entrada de veiculos, salvando os dados no banco

--- src/repository/entrada-de-veiculo-repo/entrada-de-veiculo-repo.ts
+++ src/repository/entrada-de-veiculo-repo/entrada-de-veiculo-repo.ts
@@ -9,8 +9,29 @@ export class EntradaDeVeiculoRepository implements IentradaDeVeiculoRepository {
     constructor(private readonly db: Database){}
 
     //const vagas = await this.db.all("SELECT * FROM vagas")
-    async entrada(): Promise<ticket> {
-        throw new Error("erro")
+    async entrada(placa: string): Promise<ticket> {
+
+        try{
+            const resultado = await this.db.all("SELECT id FROM vagas WHERE status='LIVRE' LIMIT 1")
+            const vaga_id = resultado[0].id
+
+            if(resultado.length === 0){
+                throw Error("Estacionamento Lotado!")
+            }
+            
+            await this.db.run("INSERT INTO tickets(placa, vaga_id, id, hora_entrada) VALUES(?, ?, ?, datetime('now', 'localtime'))", [placa, vaga_id, vaga_id])
+            await this.db.run("UPDATE vagas SET status = 'OCUPADO' WHERE id=?", [vaga_id])
+            const tickets = await this.db.all("SELECT * FROM tickets WHERE vaga_id=?",[vaga_id])
+
+            return {
+                id: tickets[0].id,
+                placa: tickets[0].placa,
+                vagaOcupada: tickets[0].vaga_id,
+                hora_de_entrada: tickets[0].hora_entrada
+            }
+        }catch{
+            throw Error("erro!")
+        }
     }
     
 }
