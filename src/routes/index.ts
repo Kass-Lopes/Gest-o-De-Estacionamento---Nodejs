aa1299a9ac26dd4d2bc93db0a8ab0339d42eea19
Kass-Lopes <kasslopes0@gmail.com>
2026-05-03 18:26:14 +0100

    Nova Rota para registo de veículos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from "express"
 import { BuscaVagasController } from "../controllers/busca-vagas/busca-vagas.controller"
 import { BuscaVagasRepository } from "../repository/busca-vagas-repo/busca-vagas-repo"
 import { conectarDB, criarTabelas } from "../config/sqlite"
+import { EntradaDeVeiculoRepository } from "../repository/entrada-de-veiculo-repo/entrada-de-veiculo-repo"
+import { EntredaDeVeiculoController } from "../controllers/entrada-de-veiculo/entrada-de-veiculo.controller"
 
 const router = Router()
 
@@ -10,13 +12,22 @@ const main =  async ()=>{
     const db = await conectarDB()
     await criarTabelas(db)
 
-    router.get("/api/vagas", async (req, res)=>{
+    router.get("/api/vagas", async (_, res)=>{
         
         const buscaVagaDB = new BuscaVagasRepository(db)
         const buscaVaga = new BuscaVagasController(buscaVagaDB)
-    
+
         const {status, data} = await buscaVaga.handle()
-        res.send({status,data})
+        res.json({status,data}).status(status)
+    })
+
+    router.post("/api/check-in/:placa", async (req, res)=>{
+        const placa = req.params.placa
+        const entradaDB = new EntradaDeVeiculoRepository(db)
+        const entradaController = new EntredaDeVeiculoController(entradaDB, placa)
+
+        const { status, data } = await entradaController.entrada()
+        res.json({status,data}).status(status)
     })
 }
 
